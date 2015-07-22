005f434089ba4895146b2b9c72e9cee0ad72cb2c
Corey Light <coreylight@gmail.com>
2015-07-22 16:26:04 -0500

    add

---
+++ src/relationships.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    name:'Equal To',
+    title:'Exactly equal to.',
+    id:'equal'
+  },
+  {
+    name:'Not Equal To',
+    title:'Not equal to.',
+    id:'notEqual'
+  },
+  {
+    name:'Is Empty',
+    title:'Is empty.',
+    id:'empty'
+  },
+  {
+    name:'Is Not Empty',
+    title:'Is not empty.',
+    id:'notEmpty'
+  },
+  {
+    name:'Contains',
+    title:'Contains the text.',
+    id:'contain'
+  },
+  {
+    name:'Does Not Contain',
+    title:'Does not contain the text.',
+    id:'notContain'
+  },
+  {
+    name:'RegExp',
+    title:'^2 etc.',
+    id:'regExp'
+  }
+]
---
+++ src/types.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name:'Status Code',
+    title:'2XX-5XX',
+    id:'statusCode'
+  },
+  {
+    name:'Header',
+    title:'Response Header, auth, etc.',
+    id:'header'
+  },
+  {
+    name:'Response Body',
+    title:'Data from the response',
+    id:'body'
+  },
+]
